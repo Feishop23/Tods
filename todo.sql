a756f9d333f174f95e44cf8847a71229eed219b4
Feishop23 <feiberth10portilla@hotmail.com>
2022-09-23 18:38:34 -0400

    primer archivo sql

---
+++ todo.sql
@@ -0,0 +1,23 @@
+
+insert into todos 
+(
+id,
+"name" ,
+designed_to,
+description,
+start_date,
+end_date ,
+is_completed 
+
+)
+
+values(
+'a782317c-63c5-49de-aefb-b6a621c8654d',
+'Feiberth',
+'Planchar la ropa',
+'Bien limpio todo porfa',
+'10/02/2022',
+true,
+true
+);
+
